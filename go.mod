ce5bb89aa54baaf84c6bb29b207e3cd938cb17ab
John Miller <john.miller@d2iq.com>
2020-05-21 12:28:09 -0400

    Pushing lsat known code

---
+++ go.mod
@@ -0,0 +1,14 @@
+module konvoyctl
+
+go 1.13
+
+require (
+	github.com/mattn/go-runewidth v0.0.8 // indirect
+	github.com/mdempsky/gocode v0.0.0-20191202075140-939b4a677f2f // indirect
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/motemen/go-quickfix v0.0.0-20200118031250-2a6e54e79a50 // indirect
+	github.com/motemen/gore v0.5.0 // indirect
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.3.2
+	golang.org/x/tools v0.0.0-20200203175837-a014e0aa6a8b // indirect
+)
--- main.go
+++ main.go
@@ -1,56 +1,11 @@
+/*
+Copyright © 2020 John Miller john.miller@d2iq.com
+
+*/
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-	"os/exec"
-	"path"
-	"strings"
-
-	"github.com/millerjem/konvoyctl/cmd"
-)
-
-var konvoyClusterPath = os.Getenv("HOME") + "/.konvoy/clusters"
+import "konvoyctl/cmd"
 
 func main() {
 	cmd.Execute()
-	//listClusters()
-	//initCluster("bah")
-	//listClusters()
 }
-
-func normalizeClusterName(name string) string {
-	return strings.ToLower(name)
-}
-
-func listClusters() {
-
-	files, err := ioutil.ReadDir(konvoyClusterPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, f := range files {
-		fmt.Println(f.Name())
-	}
-}
-
-func mkClusterDir(clusterName string) {
-	os.MkdirAll(konvoyClusterPath+"/"+normalizeClusterName(clusterName), os.ModePerm)
-}
-
-func initCluster(clusterName string) {
-	mkClusterDir(clusterName)
-	cmd := exec.Command("konvoy", "init", "--cluster-name", normalizeClusterName(clusterName))
-	location := path.Join(os.Getenv("HOME"), "/.konvoy/clusters/"+normalizeClusterName(clusterName))
-	cmd.Dir = location
-	out, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(out)
-}
-
-func provisionCLuster(clusterName string) {}
